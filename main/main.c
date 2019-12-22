38377e93997ef1d4b8e83cccb6cb98106adceb55
Ilya Shugalev <ilya@shugalev.com>
2019-12-22 11:06:25 +0300

    Activated send_to cloud

--- main/main.c
+++ main/main.c
@@ -18,14 +18,15 @@
 #include "spi_intf.h"
 //#include "shell.h"
 #include "s2lp_console.h"
+#include "cmd_nvs.h"
 #include "main.h"
 #include "S2LP_Config.h"
 #include "radio.h"
 
 
-static QueueHandle_t uart2_queue;
+//static QueueHandle_t uart2_queue;
 static uint8_t* data0;
-static uint8_t* data2;
+//static uint8_t* data2;
 static const char* TAG = "ESPwait";
 static const char* TAG1 = "SendToCloud";
 static const char *TAGW = "wifi station";
@@ -34,6 +35,11 @@ char mes1[MAX_MESSAGE_SIZE];
 char mes2[MAX_MESSAGE_SIZE];
 uint8_t con;
 tcpip_adapter_if_t ifindex;
+static input_data_t mes;
+static DRAM_ATTR xQueueHandle s2lp_evt_queue = NULL;
+static S2LPIrqs xIrqStatus;
+static wifi_config_t sta_config;
+
 
 static int s_retry_num = 0;
 /* FreeRTOS event group to signal when we are connected*/
@@ -47,14 +53,19 @@ const int WIFI_CONNECTED_BIT = BIT0;
 static void initialize_nvs()
 {
     esp_err_t err = nvs_flash_init_partition("s2lp");
-//    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-//        ESP_ERROR_CHECK( nvs_flash_erase() );
-//        err = nvs_flash_init();
-//    }
     if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Error: %s", esp_err_to_name(err));
+        ESP_LOGE(TAG, "s2lp partition error: %s", esp_err_to_name(err));
     }
+    ESP_ERROR_CHECK(err);
 
+    err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK( nvs_flash_erase() );
+        err = nvs_flash_init();
+    }
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Default partition error: %s", esp_err_to_name(err));
+    }
     ESP_ERROR_CHECK(err);
 }
 
@@ -74,80 +85,6 @@ void init_uart0()
     data0 = (uint8_t *) malloc(BUF_SIZE);
 }
 
-void init_uart2()
-{
-    uart_config_t uart_config2 = {
-        .baud_rate = 9600,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-    };
-    uart_param_config(UART_NUM_2, &uart_config2);
-    uart_set_pin(UART_NUM_2, UART2_TXD, UART2_RXD, UART2_RTS, UART2_CTS);
-    uart_driver_install(UART_NUM_2, BUF_SIZE * 2, BUF_SIZE * 2, 20, uart2_queue, 0);
-
-    data2 = (uint8_t *) malloc(BUF_SIZE);
-}
-
-
-static void uart_rec_task(void *arg)
-{
-	int n=0;
-	init_uart2();
-	uart_flush(UART_NUM_2);
-	xQueueReset(uart2_queue);
-	buf[0]=0;
-	while (1) {
-        // Read data from the UART
-        int len = uart_read_bytes(UART_NUM_2, data2, BUF_SIZE, 20 / portTICK_RATE_MS);
-        uart_write_bytes(UART_NUM_2, (const char*)data2, len);
-        if(len!=0) ESP_LOGI(TAG,"Chars received len=%d, buf=%s",len,buf);
-        for(int i=0;i<len;i++)
-        {
-        	if(data2[i]=='\r' || data2[i]=='\n')
-        	{
-        		if(n!=0)
-       			{
-        			if(memcmp(buf,"MES:",4)==0)
-        			{
-        				ESP_LOGI(TAG,"Service Message received %s",buf);
-        			}
-        			else if(memcmp(buf,"REC:",4)==0)
-        			{
-        				ESP_LOGI(TAG,"Message from device received %s",buf);
-        				if( xQueueSend( uart2_queue, ( void * )buf, ( TickType_t ) 10 ) != pdPASS )
-        				{
-        					ESP_LOGE(TAG,"QueueSend problem");
-        				}
-        			}
-        			else
-        			{
-        				ESP_LOGI(TAG,"Unknown Message received %s",buf);
-        			}
-        			n=0;
-        			buf[0]=0;
-        			uart_flush(UART_NUM_2);
-       			}
-        		continue;
-        	}
-        	buf[n++]=data2[i];
-        	buf[n]=0;
-        }
-    }
-}
-
-static void queue_watch_task(void *arg)
-{
-	while(1)
-	{
-		if(xQueueReceive(uart2_queue, mes1, 20 / portTICK_RATE_MS)==pdTRUE)
-		{
-			send_to_cloud(mes1);
-		}
-		vTaskDelay(300 / portTICK_PERIOD_MS);
-	}
-}
 
 static void event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
@@ -161,7 +98,7 @@ static void event_handler(void* arg, esp_event_base_t event_base,
     			esp_wifi_connect();
     			xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     			s_retry_num++;
-    			ESP_LOGI(TAGW, "retry to connect to the AP ssid=%s pass=%s retry=%d",CONFIG_ESP_WIFI_SSID,CONFIG_ESP_WIFI_PASSWORD,s_retry_num);
+    			ESP_LOGI(TAGW, "retry to connect to the AP ssid=%s pass=%s retry=%d",sta_config.sta.ssid,sta_config.sta.password,s_retry_num);
     		}
     		ESP_LOGI(TAGW,"connect to the AP fail");
     	}
@@ -180,24 +117,28 @@ static void event_handler(void* arg, esp_event_base_t event_base,
     }
 }
 
-static void send_to_cloud_task(void *arg)
+void send_to_cloud()
 {
-
-	while(1)
-	{
-		send_to_cloud("test");
-		vTaskDelay(1000*portTICK_PERIOD_MS);
-	}
-
+    con=1;
+    ESP_ERROR_CHECK( esp_wifi_start() );
+//    ESP_ERROR_CHECK( esp_wifi_connect() );
+   	ESP_LOGI(TAG1,"--- Begin of transfer\n");
+    while(1)
+    {
+    	ESP_LOGI(TAG1,"REC: Power: %d dbm 0x%08X 0x%08X 0x%08X\n",mes.input_signal_power,mes.data[0],mes.data[1],mes.data[2]);
+    	vTaskDelay(10000 / portTICK_PERIOD_MS);
+    	if(!xQueueReceive(s2lp_evt_queue,&mes,100/portTICK_PERIOD_MS)) break;
+    }
+   	ESP_LOGI(TAG1,"--- End of transfer\n");
+	con=0;
+    ESP_ERROR_CHECK( esp_wifi_disconnect() );
+    ESP_ERROR_CHECK( esp_wifi_stop() );
 }
-
-static DRAM_ATTR xQueueHandle s2lp_evt_queue = NULL;
-static S2LPIrqs xIrqStatus;
 
 
 static void IRAM_ATTR s2lp_intr_handler(void* arg)
 {
-	input_data data;
+	input_data_t data;
     S2LPTimerLdcIrqWa(S_ENABLE);
 	S2LPGpioIrqGetStatus(&xIrqStatus);
     if(xIrqStatus.RX_DATA_READY)
@@ -205,7 +146,7 @@ static void IRAM_ATTR s2lp_intr_handler(void* arg)
         //Get the RX FIFO size
         uint8_t cRxData = S2LPFifoReadNumberBytesRxFifo();
         //Read the RX FIFO
-        S2LPSpiReadFifo(cRxData, data.data);
+        S2LPSpiReadFifo(cRxData, (uint8_t*)data.data);
         //Flush the RX FIFO
         S2LPCmdStrobeFlushRxFifo();
         data.input_signal_power=S2LPRadioGetRssidBm();
@@ -246,9 +187,19 @@ void test_gpio(void)
 
 static void s2lp_wait(void *arg)
 {
-    input_data data;
     int32_t t=600;
     uint32_t xc=0;
+    s_wifi_event_group = xEventGroupCreate();
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    get_value_from_nvs("SSID",0,sta_config.sta.ssid);
+    get_value_from_nvs("PASSWD",0,sta_config.sta.password);
+    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
     while (1)
     {
     	if(s2lp_evt_queue==NULL)
@@ -256,9 +207,10 @@ static void s2lp_wait(void *arg)
     		vTaskDelay(100/portTICK_PERIOD_MS);
     		continue;
 		}
-    	if(xQueueReceive(s2lp_evt_queue,&data,100/portTICK_PERIOD_MS))
+    	if(xQueueReceive(s2lp_evt_queue,&mes,100/portTICK_PERIOD_MS))
         {
-        	printf("REC: Power: %d dbm 0x%08X 0x%08X 0x%08X\n",data.input_signal_power,data.data[0],data.data[1],data.data[2]);
+//        	printf("REC: Power: %d dbm 0x%08X 0x%08X 0x%08X\n",data.input_signal_power,data.data[0],data.data[1],data.data[2]);
+        	send_to_cloud();
         };
         t--;
         if(t<=0)
@@ -312,63 +264,13 @@ static void s2lp_rec_start(void *arg)
 
     ESP_LOGI(TAG,"s2lp irq Status get");
     S2LPCmdStrobeRx();
-    s2lp_evt_queue = xQueueCreate(10, sizeof(input_data));
-/*    input_data data;
-    int32_t t=600;
-    uint32_t xc=0;
-    while (1)
-    {
-    	if(xQueueReceive(s2lp_evt_queue,&data,100/portTICK_PERIOD_MS))
-        {
-        	printf("REC: Power: %d dbm 0x%08X 0x%08X 0x%08X\n",data.input_signal_power,data.data[0],data.data[1],data.data[2]);
-        };
-        t--;
-        if(t<=0)
-        {
-        	ESP_LOGI(TAG,"i am alive 0x%08X\n",xc);
-        	t=600;
-        	xc++;
-        }
-    }*/
+    s2lp_evt_queue = xQueueCreate(10, sizeof(input_data_t));
     while(1) vTaskDelay(60000);
-}
-
-void send_to_cloud(char* mes)
-{
-	s_retry_num=0;
-	ESP_LOGI(TAG1,"Message from device received %d %s",strlen(mes),mes);
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
-    s_wifi_event_group = xEventGroupCreate();
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = CONFIG_ESP_WIFI_SSID,
-            .password = CONFIG_ESP_WIFI_PASSWORD,
-//            .bssid_set = false
-        }
-    };
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-    con=1;
-    ESP_ERROR_CHECK( esp_wifi_start() );
-//    ESP_ERROR_CHECK( esp_wifi_connect() );
-	vTaskDelay(10000 / portTICK_PERIOD_MS);
-	con=0;
-    ESP_ERROR_CHECK( esp_wifi_disconnect() );
-    ESP_ERROR_CHECK( esp_wifi_stop() );
-    ESP_ERROR_CHECK( esp_wifi_deinit() );
-    vEventGroupDelete(s_wifi_event_group);
-    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, event_handler));
-    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, event_handler));
 }
 
 void app_main(void)
 {
 	initialize_nvs();
-    tcpip_adapter_init();
     init_uart0();
 //    ESP_ERROR_CHECK(esp_event_loop_create_default());
 //    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
--- main/main.h
+++ main/main.h
@@ -29,21 +29,21 @@
 
 #define PACKETLEN 12
 
-void init_uart0();
-void init_uart2();
-static void uart_rec_task(void *arg);
-void send_to_cloud(char* mes);
-static void queue_watch_task(void *arg);
-static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
-static void IRAM_ATTR s2lp_intr_handler(void* arg);
-static void s2lp_wait(void *arg);
-void test_gpio(void);
-
 typedef struct
 {
 	int32_t input_signal_power;
 	uint32_t data[PACKETLEN/4];
-} input_data;
+} input_data_t;
+
+void init_uart0();
+//void init_uart2();
+//static void uart_rec_task(void *arg);
+void send_to_cloud(void);
+//static void queue_watch_task(void *arg);
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
+static void IRAM_ATTR s2lp_intr_handler(void* arg);
+static void s2lp_wait(void *arg);
+void test_gpio(void);
 
 
 #endif /* MAIN_MAIN_H_ */
